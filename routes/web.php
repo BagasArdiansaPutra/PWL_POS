df316a2d998f835c333317fdcfec3d3ff08519c2
BagasArdiansaPutra <bagasardiansa06.com>
2026-02-22 23:24:08 +0700

    prakt 5

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +8,4 @@ Route::get('/', function () {
 });
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
