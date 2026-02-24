973db4c586edc382b04dc6794a42a93f8b987f27
BagasArdiansaPutra <bagasardiansa06.com>
2026-02-24 10:55:09 +0700

    modified:   app/Http/Controllers/KategoriController.php 	modified:   app/Http/Controllers/LevelController.php 	new file:   app/Http/Controllers/UserController.php 	new file:   app/Models/UserModel.php 	modified:   database/migrations/2026_02_22_144118_create_m_kategori_table.php 	modified:   database/migrations/2026_02_22_145024_create_m_user_table.php 	renamed:    database/migrations/2026_02_22_143542_create_m_level_table.php -> database/migrations/2026_02_24_010245_create_m_level_tabel.php 	modified:   database/seeders/LevelSeeder.php 	modified:   database/seeders/PenjualanDetailSeeder.php 	modified:   database/seeders/UserSeeder.php 	modified:   resources/views/kategori.blade.php 	modified:   resources/views/level.blade.php 	new file:   resources/views/user.blade.php 	modified:   routes/web.php

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -1,10 +1,7 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-
 class KategoriController extends Controller
 {
     public function index()
@@ -16,9 +13,8 @@ class KategoriController extends Controller
         'created_at' => now()
     ];
     DB::table('m_kategori')->insert($data);
-    return 'Insert data baru berhasil';
-    */
-
+    return 'Insert data baru berhasil'; */
+    
     // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->update(['kategori_nama' => 'Camilan']);
     // return 'Update data berhasil. Jumlah data yang diupdate : ' . $row . ' baris';
 
--- resources/views/kategori.blade.php
+++ resources/views/kategori.blade.php
@@ -5,14 +5,12 @@
 </head>
 <body>
     <h1>Data Kategori Barang</h1>
-
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
             <th>ID</th>
             <th>Kode Kategori</th>
             <th>Nama Kategori</th>
         </tr>
-
         @foreach ($data as $d)
         <tr>
             <td>{{ $d->kategori_id }}</td>
