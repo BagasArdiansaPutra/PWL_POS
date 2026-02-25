999d36b360d941f02171474ef1ae93e16e40ec50
BagasArdiansaPutra <bagasardiansa06.com>
2026-02-25 16:21:49 +0700

    jobsheet 4 prakt 1&2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,19 +1,16 @@
 <?php
 namespace App\Http\Controllers;
-use App\Models\UserModel;
 use Illuminate\Http\Request;
+use App\Models\UserModel;
 use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index()
     {
-        // tambah data user dengan Eloquent Model
-        $data = [
-            'nama' => 'Pelanggan Pertama',
-        ];
-        UserModel::where('username', 'customer-1')->update($data); // update data user
-        // coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
+
         return view('user', ['data' => $user]);
     }
 }
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,7 +5,12 @@ use Illuminate\Database\Eloquent\Model;
 class UserModel extends Model
 {
     use HasFactory;
-
-    protected $table = 'm_user';        // Mendefinisikan nama tabel
-    protected $primaryKey = 'user_id';  // Mendefinisikan primary key
+    protected $table = 'm_user';
+    protected $primaryKey = 'user_id';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['level_id', 'username', 'nama'];
 }
